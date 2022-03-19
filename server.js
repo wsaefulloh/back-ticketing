07bd050c1a186d99a0d406e2f33b2dc503064fea
Wahyu Saefulloh <saefuuloh@gmail.com>
2022-03-19 23:12:20 +0700

    change .env

--- server.js
+++ server.js
@@ -3,16 +3,13 @@
 const server = require('./app');
 const db = require('./src/Config/dbConnec');
 
-const PORT = 8000;
 // const orm = require('./src/Config/dbConnec');
 
 const run = async () => {
   try {
     await db.authenticate();
     await db.sync({ alter: true });
-    server.listen(PORT, () => {
-      console.log(`Service running on port ${PORT}`);
-    });
+    server.listen(process.env.PORT || 3000);
   } catch (error) {
     console.log(error);
   }
